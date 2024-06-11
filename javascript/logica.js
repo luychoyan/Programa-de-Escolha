707102b5d35fea5d29592d7d7f4529a194da1c86
Luy_ <choyanluy@gmail.com>
2024-06-11 04:13:35 -0300

    nova aplicacao

---
+++ javascript/logica.js
@@ -0,0 +1,17 @@
+
+let opcao1 = document.getElementById('escolha1')
+let opcao2 = document.getElementById('escolha2')
+let pessoa1 = document.getElementById('pessoa1')
+let pessoa2 = document.getElementById('pessoa2')
+let pessoa3 = document.getElementById('pessoa3')
+let pessoa4 = Document.getElementById('pessoa4')
+let pessoa5 = document.getElementById('pessoa5')
+let pessoa6 = document.getElementById('pessoa6')
+let pessoa7 = document.getElementById('pessoa7')
+let resposta = Document.getElementById('resposta')
+
+if (opcao1 == true) {
+    function selecionar() {
+        resposta = document.write(pessoa1)
+    }
+}
